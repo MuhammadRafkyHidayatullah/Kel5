604968a3ee345189950d377a089ccec3f87642e8
bayy1390 <vambb21@gmail.com>
2024-10-03 14:41:16 +0700

    rawat update

--- app/Models/rawat.php
+++ app/Models/rawat.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class rawat extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+    protected $fillable = ["tanggal_masuk","tanggal_keluar", "ruang_id","pasien_id"];
+
+    public function ruang(){
+        return $this->belongsTo(ruang::class, 'ruang_id');
+    }
+    public function pasien(){
+        return $this->belongsTo(pasien::class, 'pasien_id');
+    }
 }
