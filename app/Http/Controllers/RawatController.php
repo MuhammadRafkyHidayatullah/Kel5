6c1171ce0bc0e21b2ffa1887be26fbf4601d7e93
bayubazra <vambb21@gmail.com>
2024-10-03 08:02:59 +0700

    rawat

--- app/Http/Controllers/RawatController.php
+++ app/Http/Controllers/RawatController.php
@@ -4,62 +4,85 @@ namespace App\Http\Controllers;
 
 use App\Models\rawat;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class RawatController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+        $rawat = rawat::with(['ruang', 'pasien'])->get();
+        $data['message'] = true;
+        $data['result'] = $rawat;
+        return response()->json($data, Response::HTTP_OK);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
-    {
-        //
+    {      
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'jadwal_masuk' => 'required',
+            'jadwal_keluar' => 'required',
+            'ruang_id' => 'required',
+            'pasien_id' => 'required'
+        ]);
+
+        $result = rawat::create($validate); 
+        if($result){
+            $data['success'] = true;
+            $data['message'] = "Data prodi berhasil disimpan";
+            $data['result'] = $result;
+            return response()->json($data, Response::HTTP_CREATED);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(rawat $rawat)
-    {
-        //
+    {      
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(rawat $rawat)
-    {
-        //
+    {   
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, rawat $rawat)
+    
+    public function update(Request $request, $id)
     {
-        //
+        $validate = $request->validate([
+            'jadwal_masuk' => 'required',
+            'jadwal_keluar' => 'required',
+            'ruang_id' => 'required',
+            'pasien_id' => 'required'
+        ]);
+
+        $result = rawat::where('id', $id)->update($validate);
+        if($result){
+            $data['success'] = true;
+            $data['message'] = "Data rawat berhasil diupdate";
+            $data['result'] = $result;
+            return response()->json($data, Response::HTTP_OK);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(rawat $rawat)
     {
-        //
+        $rawat = rawat::find($id);
+        if($rawat){
+            $rawat->delete(); 
+            $data['success'] = true;
+            $data['message'] = "Data rawat berhasil dihapus";
+            return response()->json($data, Response::HTTP_OK);
+        } else {
+            $data['success'] = false;
+            $data['message'] = "Data ra$rawat tidak ditemukan";
+            return response()->json($data, Response::HTTP_NOT_FOUND);
+        }
     }
 }
