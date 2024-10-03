6da553d097ef84ae70fad7f0b4e3532a3375d80b
MuhammadRafkyHidayatullah <146694847+MuhammadRafkyHidayatullah@users.noreply.github.com>
2024-10-03 07:26:19 +0700

    api Pasien

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\dokterController;
+use App\Http\Controllers\pasienController;
+use App\Http\Controllers\ruangController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/dokter', [dokterController::class, 'index']);
+Route::post('/dokter', [dokterController::class, 'store' ]);
+Route::patch('/dokter/{dokter}', [dokterController::class, 'update']);
+Route::delete('/dokter/{dokter}', [dokterController::class, 'destroy']);
+
+Route::get('/ruang', [ruangController::class, 'index']);
+Route::post('/ruang', [ruangController::class, 'store']);
+Route::patch('/ruang/{ruang}', [ruangController::class, 'update']);
+Route::delete('/ruang/{ruang}', [ruangController::class, 'destroy']);
+
+Route::get('/pasien', [pasienController::class,'index']);
+Route::post('/pasien', [pasienController::class,'store']);
+Route::delete('/pasien/{pasien}', [pasienController::class,'update']);
+Route::delete('/pasien/{pasien}', [pasienController::class,'destroy']);
+
